fbbd5b3d30f273fbde62c6ddf2fdc1c3783237ed
Ismael Guillermo Marin Cabrera <ismael.marin@gmail.com>
2021-02-26 13:12:13 -0600

    Basic CRUD server

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 // User is (this line is for the linter)
 type User struct {
 	ID        int
@@ -12,13 +17,52 @@ var (
 	nextID = 1
 )
 
-func getUsers() []*User {
+// GetUsers All users as an array
+func GetUsers() []*User {
 	return users
 }
 
-func addUser(u User) (User, error) {
+// AddUser add user and incremenet ID if not present
+func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New user must include ID or most be set")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+// GetUserByID by ID
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", id)
+}
+
+// UpdateUser by ID
+func UpdateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u
+			return u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
+}
+
+// RemoveUserByID by ID
+func RemoveUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' not found", id)
 }
